d04accd4bdcfb72d8fcce196a7e91300ed2105c2
Serhii <sergey.mov8@gmail.com>
2021-11-01 15:54:21 +0200

    [HW-11-01] Modified Fifth

--- lesson11/src/data/Fifth.java
+++ lesson11/src/data/Fifth.java
@@ -6,10 +6,11 @@ package data;
 */
 public class Fifth {
     public static void main(String[] arg) {
-        boolean a = true;
+        boolean a = true;    // Объявление переменной a
         boolean b = false;
         boolean c = true;
         boolean result = a && (b || c);
+        // Вывод результата на печать
         System.out.println(result);
     }
 }
