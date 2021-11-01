c94e3de7a784ca903556512180ce1c52e71736a7
Serhii <sergey.mov8@gmail.com>
2021-11-01 14:39:37 +0200

    [HW-11-01] Modified Third

--- lesson11/src/data/Third.java
+++ lesson11/src/data/Third.java
@@ -4,7 +4,7 @@ public class Third {
     public static void main(String[] arg) {
         int a = 12;
         int b = 34;
-        int c = b / a;
+        int c = b % a;
         System.out.println(c);
     }
 }
