5268c5a528fa7af39f377a1873010b93f1b7dc44
Serhii <sergey.mov8@gmail.com>
2021-11-01 14:33:21 +0200

    [HW-11-01] Created Third in data package

---
+++ lesson11/src/data/Third.java
@@ -0,0 +1,10 @@
+package data;
+
+public class Third {
+    public static void main(String[] arg) {
+        int a = 12;
+        int b = 34;
+        int c = b / a;
+        System.out.println(c);
+    }
+}
