f12d0a24612277354723f17085808b1d5e29c3b5
Serhii <sergey.mov8@gmail.com>
2021-11-04 20:46:06 +0200

    [HW-11-01] Modified "Robot" class in "oop" package

--- lesson11/src/oop/Robot.java
+++ lesson11/src/oop/Robot.java
@@ -1,5 +1,10 @@
 package oop;
 
 public class Robot {
-
+    // Текущая координата X
+    int x = 0;
+    // Текущая координата Y
+    int y = 0;
+    // Текущий курс (в градусах)
+    int course = 0;
 }
