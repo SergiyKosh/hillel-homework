db6ce39d016889636f5e9c14d089d1539cf7037c
Serhii <sergey.mov8@gmail.com>
2021-11-04 20:51:00 +0200

    [HW-11-01] Modified "RobotManager" class in "oop" package

--- lesson11/src/oop/RobotManager.java
+++ lesson11/src/oop/RobotManager.java
@@ -3,7 +3,7 @@ package oop;
 public class RobotManager {
     public static void main(String[] args) {
         Robot r1 = new Robot();
-        Robot r2;
-        r2 = new Robot();
+        r1.x = 99;
+        System.out.println(r1.x);
     }
 }
