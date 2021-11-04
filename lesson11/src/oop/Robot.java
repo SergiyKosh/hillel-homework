79705073df2c2d0b7cf3fd1c6e2e72ba8355e10d
Serhii <sergey.mov8@gmail.com>
2021-11-04 20:56:10 +0200

    [HW-11-01] Modified "RobotManager" class in "oop" package

--- lesson11/src/oop/Robot.java
+++ lesson11/src/oop/Robot.java
@@ -7,4 +7,13 @@ public class Robot {
     int y = 0;
     // Текущий курс (в градусах)
     int course = 0;
+
+    void printCoordinates() {
+        System.out.println(x + "," + y);
+    }
+
+    void forward(int distance) {
+        x = (int) (x + distance * Math.cos(course / 180 * Math.PI));
+        y = (int) (y + distance * Math.sin(course / 180 * Math.PI));
+    }
 }
--- lesson11/src/oop/RobotManager.java
+++ lesson11/src/oop/RobotManager.java
@@ -2,11 +2,14 @@ package oop;
 
 public class RobotManager {
     public static void main(String[] args) {
-        Robot r1 = new Robot();
-        Robot r2 = new Robot();
-        r1.x = 99;
-        r2.x = 123;
-        System.out.println(r1.x);
-        System.out.println(r2.x);
+        Robot robot = new Robot();
+        robot.forward(20);
+        robot.printCoordinates();
+        robot.course = 90;
+        robot.forward(20);
+        robot.printCoordinates();
+        robot.course = 45;
+        robot.forward(20);
+        robot.printCoordinates();
     }
 }
