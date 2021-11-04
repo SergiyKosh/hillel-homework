0172b73d76fa748ae4897a102acc087275d9dc54
Serhii <sergey.mov8@gmail.com>
2021-11-04 17:28:21 +0200

    [HW-11-01] Modified "CheckFirst" class

--- lesson11/src/operations/CheckFirst.java
+++ lesson11/src/operations/CheckFirst.java
@@ -4,10 +4,12 @@ public class CheckFirst {
     public static void main(String[] arg) {
         int count = 10;
         int i = 0;
+        // Обычный цикл while для вывода строки
         while (i++ < count) {
             System.out.print("*");
         }
-        while (++i < count) ;
+        // Цикл while с пустым оператором
+        while (++i < count) ;  // ; - и есть пустой оператор
         System.out.println();
     }
 }
