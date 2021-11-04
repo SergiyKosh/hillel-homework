18f5fabec58256bc9ba9929be516523a4fd67cff
Serhii <sergey.mov8@gmail.com>
2021-11-04 17:33:58 +0200

    [HW-11-01] Created "ContinueFirst" class in "operations" package

---
+++ lesson11/src/operations/ContinueFirst.java
@@ -0,0 +1,12 @@
+package operations;
+
+public class ContinueFirst {
+    public static void main(String[] arg) {
+        for (int i = 0; i < 20; i++) {
+            if (i % 2 == 0) {    // число четное -
+                continue;       // переходим в начало цикла
+            }
+            System.out.println("Number=" + i);   // оператор исполняется только для нечетных чисел
+        }
+    }
+}
