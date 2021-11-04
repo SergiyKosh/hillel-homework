e6b2b13414bcfa749dc3b6776535bcdb1558014a
Serhii <sergey.mov8@gmail.com>
2021-11-05 00:25:18 +0200

    [HW-11-01] Modified "RobotTotal" class in the "override_overload" package

---
+++ lesson11/src/override_overload/RobotTotal.java
@@ -0,0 +1,39 @@
+package override_overload;
+
+import oop.Robot;
+
+public class RobotTotal extends Robot {
+    //    private double totalDistance = 0;
+//    public RobotTotal(double x, double y) {
+//        super(x, y);
+//    }
+//
+//    @Override
+//    public void forward(int distance) {
+//        super.forward(distance);
+//        totalDistance += distance;
+//    }
+//
+//    public double getTotalDistance() {
+//        return totalDistance;
+//    }
+    private double totalDistance = 0;
+
+    public RobotTotal() {
+        super(0, 0);
+    }
+
+    public RobotTotal(double x, double y) {
+        super(x, y);
+    }
+
+    @Override
+    public void forward(int distance) {
+        super.forward(distance);
+        totalDistance += distance;
+    }
+
+    public double getTotalDistance() {
+        return totalDistance;
+    }
+}
