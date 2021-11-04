47832065439c5608a909fb0b2ae98365418ef5c7
Serhii <sergey.mov8@gmail.com>
2021-11-04 13:37:35 +0200

    [HW-11-01] Created "LoopDoTwo" class in the "operations" package

---
+++ lesson11/src/operations/LoopDoTwo.java
@@ -0,0 +1,12 @@
+package operations;
+
+public class LoopDoTwo {
+    public static void main(String[] arg) {
+        int count = 10;
+        int i = 0;
+        do {
+            System.out.print("*");
+        } while (++i < count);   // Вот здесь наш оператор ++
+        System.out.println();
+    }
+}
