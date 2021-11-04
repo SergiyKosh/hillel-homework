0b92ee1772d33b2272555b94b80d7b2b279ba5dd
Serhii <sergey.mov8@gmail.com>
2021-11-04 22:57:30 +0200

    [HW-11-01] Modified "RobotExt" class in the "inheritance" package

--- lesson11/src/inheritance/RobotExt.java
+++ lesson11/src/inheritance/RobotExt.java
@@ -3,8 +3,9 @@ package inheritance;
 import oop.Robot;
 
 public class RobotExt extends Robot {
-    public RobotExt(double x, double y) {
+    public RobotExt(double x, double y, double course) {
         super(x, y);
+        setCourse(course);
     }
 
     public void back(int distance) {
