823ad4a814b428c5621ee5ba2ce5ed2042dca734
Serhii <sergey.mov8@gmail.com>
2021-11-04 23:14:17 +0200

    [HW-11-01] Modified "SimpleFrame" class in the "packages" package

--- lesson11/src/packages/SimpleFrame.java
+++ lesson11/src/packages/SimpleFrame.java
@@ -4,10 +4,10 @@ import javax.swing.JFrame;
 
 public class SimpleFrame {
     public static void main(String[] args) {
-        JFrame sf = new JFrame();
+        javax.swing.JFrame sf = new javax.swing.JFrame();
         sf.setTitle("First window");
-        sf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        sf.setBounds(100,100,400,200);
+        sf.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+        sf.setBounds(100, 100, 400, 200);
         sf.setVisible(true);
     }
 }
