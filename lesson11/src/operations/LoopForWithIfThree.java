a04bed063266d0737a195e3dca76ea12f2d6a726
Serhii <sergey.mov8@gmail.com>
2021-11-04 20:35:54 +0200

    [HW-11-01] Created LoopForWithIfThree class in operations package (4th figure)

--- lesson11/src/operations/LoopForWithIfThree.java
+++ lesson11/src/operations/LoopForWithIfThree.java
@@ -3,9 +3,12 @@ package operations;
 public class LoopForWithIfThree {
     public static void main(String[] arg) {
         int count = 11;
-        for (int i = 0; i < count; i++) {
+        int j = -1 * (count / 2) + 1;
+        for (int i = 0; i < count; i++, j++) {
             for (int k = 0; k < count - i + count - 1; k++) {
-                if ((i == count - count / 2 - 1 && k < count) || (i == 0 && k == count / 2) || k == count / 2 - i || k == count / 2 + i && i < count / 2) {
+                if ((i == 0 && k == count / 2) || k == count / 2 - i || k == count / 2 + i && i <= count / 2
+                        || (i == count - 1 && k == count / 2) || k == i - count / 2
+                        || i >= count / 2 && (k > count / 2 && i > count / 2) && k == count - j) {
                     System.out.print("*");
                 } else {
                     System.out.print(" ");
