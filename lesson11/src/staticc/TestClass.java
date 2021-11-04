ae2b94e58436ddd00a1a69216df529ac5aa519d8
Serhii <sergey.mov8@gmail.com>
2021-11-05 01:17:56 +0200

    [HW-11-01] Modified "TestClass" class in the "staticc" package

--- lesson11/src/staticc/TestClass.java
+++ lesson11/src/staticc/TestClass.java
@@ -1,7 +1,10 @@
 package staticc;
 
 public class TestClass {
+    private static Integer staticValue = 99;
+    private Integer simpleValue = 99;
+
     public static void main(String[] arg) {
-        System.out.println("Hello, world");
+        System.out.println(TestClass.staticValue);
     }
 }
