0d641c6ead347b05373613f0ae94ab234db4cb10
Serhii <sergey.mov8@gmail.com>
2021-11-05 00:37:44 +0200

    [HW-11-01] Created "OvalComponent", "OvalFrame", "OvalApplication" classes in the "override_overload" package

---
+++ lesson11/src/override_overload/OvalComponent.java
@@ -0,0 +1,12 @@
+package override_overload;
+
+import java.awt.Graphics;
+import javax.swing.JComponent;
+
+public class OvalComponent extends JComponent {
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawOval(5, 5, getWidth() - 10, getHeight() - 10);
+    }
+}
