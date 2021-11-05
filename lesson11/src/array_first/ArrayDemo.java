71281b01cebc9534726552541f7d239d99136121
Serhii <sergey.mov8@gmail.com>
2021-11-05 16:58:56 +0200

    [HW-11-01] Created "ArrayDemo" class in the "array_first" package

---
+++ lesson11/src/array_first/ArrayDemo.java
@@ -0,0 +1,14 @@
+package array_first;
+
+public class ArrayDemo {
+    public static void main(String[] args) {
+        int[] demo = new int[10];
+        for (int i = 0; i < demo.length; i++) {
+            System.out.println(demo[i]);
+            demo[i] = 10 * (i + 1);
+        }
+        for (int i = 0; i < demo.length; i++) {
+            System.out.println(demo[i]);
+        }
+    }
+}
