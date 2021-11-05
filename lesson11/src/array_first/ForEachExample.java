3fa28791a99f9fd3cd4486a213ab984d4d903635
Serhii <sergey.mov8@gmail.com>
2021-11-05 17:06:16 +0200

    [HW-11-01] Modified "ForEachExample" class in the "array_first" package

--- lesson11/src/array_first/ForEachExample.java
+++ lesson11/src/array_first/ForEachExample.java
@@ -2,10 +2,28 @@ package array_first;
 
 public class ForEachExample {
     public static void main(String[] args) {
-        int[] sample = {12, 56, 7, 34, 89, 43, 23, 9};
+//        int[] sample = {12, 56, 7, 34, 89, 43, 23, 9};
+//
+//        for (int x : sample) {
+//            System.out.println(x);
+//        }
 
-        for (int x : sample) {
-            System.out.println(x);
+        int[] sample = new int[5];
+
+        System.out.println("До foreach");
+
+        for (int t : sample) {
+            System.out.println(t);
+        }
+
+        for (int t : sample) {
+            t = 99;
+        }
+
+        System.out.println("После foreach");
+
+        for (int t : sample) {
+            System.out.println(t);
         }
     }
 }
