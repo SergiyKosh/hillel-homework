752aeec8cbf1f38851a55fc59e11bc11fc314e0e
Serhii <sergey.mov8@gmail.com>
2021-11-05 17:05:30 +0200

    [HW-11-01] Created "ForEachExample" class in the "array_first" package

---
+++ lesson11/src/array_first/ForEachExample.java
@@ -0,0 +1,11 @@
+package array_first;
+
+public class ForEachExample {
+    public static void main(String[] args) {
+        int[] sample = {12, 56, 7, 34, 89, 43, 23, 9};
+
+        for (int x : sample) {
+            System.out.println(x);
+        }
+    }
+}
