f565b177e7e59f4277025e2012c8aae8ec631b13
Serhii <sergey.mov8@gmail.com>
2021-11-05 18:36:58 +0200

    [HW-12-02] Created "Dog" class

---
+++ lesson12/hw2/src/entity/Dog.java
@@ -0,0 +1,13 @@
+package entity;
+
+public class Dog extends Animal {
+
+    public Dog(String name) {
+        super(name);
+    }
+
+    @Override
+    public String getName() {
+        return "Я собака. Зовут меня - " + super.getName();
+    }
+}
