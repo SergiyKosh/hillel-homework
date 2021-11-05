a87866c80fc3d299698e60e5fa5994523547fd7f
Serhii <sergey.mov8@gmail.com>
2021-11-05 17:38:11 +0200

    [HW-11-01] Created "AbstractShape" abstract class in the "abstractt" package

---
+++ lesson11/src/abstractt/AbstractShape.java
@@ -0,0 +1,14 @@
+package abstractt;
+
+import java.awt.Graphics;
+import javax.swing.JComponent;
+
+public abstract class AbstractShape extends JComponent {
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        paintShape(g);
+    }
+
+    abstract protected void paintShape(Graphics g);
+}
