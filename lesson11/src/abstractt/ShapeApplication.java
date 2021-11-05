9cabd7f34b01fb48ede917f749878caaccbcb1f2
Serhii <sergey.mov8@gmail.com>
2021-11-05 17:44:58 +0200

    [HW-11-01] Created "ShapeFrame" and "ShapeApplication" classes in the "abstractt" package

---
+++ lesson11/src/abstractt/ShapeApplication.java
@@ -0,0 +1,11 @@
+package abstractt;
+
+import javax.swing.JFrame;
+
+public class ShapeApplication {
+    public static void main(String[] args) {
+        ShapeFrame of = new ShapeFrame();
+        of.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        of.setVisible(true);
+    }
+}
