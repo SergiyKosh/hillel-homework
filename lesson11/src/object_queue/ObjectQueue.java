3aeb9507c1975b8bf481a98f9e37872c0208844a
Serhii <sergey.mov8@gmail.com>
2021-11-05 15:55:25 +0200

    [HW-11-01] Created "ObjectBox" class in the "ObjectQueue" class

--- lesson11/src/object_queue/ObjectQueue.java
+++ lesson11/src/object_queue/ObjectQueue.java
@@ -12,4 +12,25 @@ public class ObjectQueue {
     public int size() {
 
     }
+
+    private class ObjectBox {
+        private Object object;
+        private ObjectBox next;
+
+        public Object getObject() {
+            return object;
+        }
+
+        public void setObject(Object object) {
+            this.object = object;
+        }
+
+        public ObjectBox getNext() {
+            return next;
+        }
+
+        public void setNext(ObjectBox next) {
+            this.next = next;
+        }
+    }
 }
