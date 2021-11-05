a7c9d93451b64ade4d0a083543db26de7cd4c7db
Serhii <sergey.mov8@gmail.com>
2021-11-05 17:53:06 +0200

    [HW-11-01] Modified "Robot" class in the "oop" package

--- lesson11/src/oop/Robot.java
+++ lesson11/src/oop/Robot.java
@@ -1,5 +1,6 @@
 package oop;
 
+import interfaces.RobotListener;
 import relations.Operator;
 import robot_visual.RobotLine;
 
@@ -163,22 +164,78 @@ public class Robot {
 //    public ArrayList<RobotLine> getLines() {
 //        return lines;
 //    }
+//    private double x = 0;
+//    private double y = 0;
+//    protected double course = 0;
+//
+//    public Robot(double x, double y) {
+//        this.x = x;
+//        this.y = y;
+//    }
+//
+//    public void forward(int distance) {
+//        x = x + distance * Math.cos(course / 180 * Math.PI);
+//        y = y + distance * Math.sin(course / 180 * Math.PI);
+//    }
+//
+//    public void printCoordinates() {
+//        System.out.println(x + "," + y);
+//    }
+//
+//    public double getX() {
+//        return x;
+//    }
+//
+//    public double getY() {
+//        return y;
+//    }
+//
+//    public double getCourse() {
+//        return course;
+//    }
+//
+//    public void setCourse(double course) {
+//        this.course = course;
+//    }
+
     private double x = 0;
     private double y = 0;
     protected double course = 0;
+    private ArrayList<RobotLine> lines = new ArrayList<RobotLine>();
+    // Ссылка на слушателя событий от робота
+    // Обратите внимание, что это ссылка на ИНТЕРФЕЙС
+    private RobotListener listener;
 
     public Robot(double x, double y) {
         this.x = x;
         this.y = y;
     }
 
-    public void forward(int distance) {
-        x = x + distance * Math.cos(course / 180 * Math.PI);
-        y = y + distance * Math.sin(course / 180 * Math.PI);
+    // Метод для установки реального слушателя.
+    public void setListener(RobotListener listener) {
+        this.listener = listener;
     }
 
-    public void printCoordinates() {
-        System.out.println(x + "," + y);
+    public void forward(int distance) {
+        // Вызываем слушателя (если он установлен) в начале
+        if(listener !=null) {
+            listener.startMove(x, y);
+        }
+        // Запоминаем координаты робота перед перемещением
+        final double xOld = x;
+        final double yOld = y;
+        // Меняем координаты
+        x += distance * Math.cos(course / 180 * Math.PI);
+        y += distance * Math.sin(course / 180 * Math.PI);
+
+        // Вызываем слушателя (если он установлен) после остановки
+        if(listener !=null) {
+            listener.endMove(x, y);
+        }
+
+        // Запоминаем координаты пройденного пути в списке
+        // Класс List позволяет добавить объект и хранить его
+        lines.add(new RobotLine(xOld, yOld, x, y));
     }
 
     public double getX() {
@@ -195,5 +252,9 @@ public class Robot {
 
     public void setCourse(double course) {
         this.course = course;
+    }
+
+    public ArrayList<RobotLine> getLines() {
+        return lines;
     }
 }
