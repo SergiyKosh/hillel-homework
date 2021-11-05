0eb92e0c47436004747bb5f538de3d5619689db1
Serhii <sergey.mov8@gmail.com>
2021-11-05 17:54:49 +0200

    [HW-11-01] Modified "SimpleRobotListener" class in the "interfaces" package

--- lesson11/src/interfaces/SimpleRobotListener.java
+++ lesson11/src/interfaces/SimpleRobotListener.java
@@ -1,4 +1,13 @@
 package interfaces;
 
-public class SimpleRobotListener implements RobotListener{
+public class SimpleRobotListener implements RobotListener {
+    @Override
+    public void startMove(double x, double y) {
+        System.out.println("Робот начал движение, координаты:" + x + "," + y);
+    }
+
+    @Override
+    public void endMove(double x, double y) {
+        System.out.println("Робот закончил движение, координаты:" + x + "," + y);
+    }
 }
