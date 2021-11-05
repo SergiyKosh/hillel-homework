6db72d8a30c510b72c3cadf67d8fb70db1d7755d
Serhii <sergey.mov8@gmail.com>
2021-11-05 18:36:52 +0200

    [HW-12-02] Created "Animal" class

---
+++ lesson12/hw2/src/entity/Animal.java
@@ -0,0 +1,13 @@
+package entity;
+
+public class Animal {
+    private String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
