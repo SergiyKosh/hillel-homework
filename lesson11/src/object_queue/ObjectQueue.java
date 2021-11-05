78ccc398ff70789b4fc569f3fd952b6f7ed03edc
Serhii <sergey.mov8@gmail.com>
2021-11-05 16:05:35 +0200

    [HW-11-01] Modified "ObjectQueue" class

--- lesson11/src/object_queue/ObjectQueue.java
+++ lesson11/src/object_queue/ObjectQueue.java
@@ -1,16 +1,37 @@
 package object_queue;
 
 public class ObjectQueue {
-    public void push(Object obj) {
+    private ObjectBox head = null;
+    private ObjectBox tail = null;
+    private int size = 0;
 
+    public void push(Object obj) {
+        ObjectBox ob = new ObjectBox();
+        ob.setObject(obj);
+        if (head == null) {
+            head = ob;
+        } else {
+            tail.setNext(ob);
+        }
+        tail = ob;
+        size++;
     }
 
     public Object pull() {
-
+        if (size == 0) {
+            return null;
+        }
+        Object obj = head.getObject();
+        head = head.getNext();
+        if (head == null) {
+            tail = null;
+        }
+        size--;
+        return obj;
     }
 
     public int size() {
-
+        return size;
     }
 
     private class ObjectBox {
