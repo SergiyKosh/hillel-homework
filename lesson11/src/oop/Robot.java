7b0378baf7f7c7fe9ed42fd4908243516225aa71
Serhii <sergey.mov8@gmail.com>
2021-11-05 15:00:28 +0200

    [HW-11-01] Modified "Robot" class and created "Operator" class

--- lesson11/src/oop/Robot.java
+++ lesson11/src/oop/Robot.java
@@ -1,7 +1,9 @@
 package oop;
 
+import relations.Operator;
+
 public class Robot {
-//    private double x = 0;
+    //    private double x = 0;
 //    private double y = 0;
 //    protected double course = 0;
 //    private String name;
@@ -44,24 +46,60 @@ public class Robot {
 //    public String toString() {
 //        return "name: " + name;
 //    }
-private double x = 0;
+//    private double x = 0;
+//    private double y = 0;
+//    protected double course = 0;
+//
+//    public Robot(double x, double y) {
+//        this.x = x;
+//        this.y = y;
+//    }
+//
+//    public void forward(int distance) {
+//        x = x + distance * Math.cos(course / 180 * Math.PI);
+//        y = y + distance * Math.sin(course / 180 * Math.PI);
+//    }
+//
+//    public void printCoordinates() {
+//        System.out.println(x + "," + y);
+//    }
+//
+//    public double getX() {
+//        return x;
+//    }
+//
+//    public double getY() {
+//        return y;
+//    }
+//
+//    public double getCourse() {
+//        return course;
+//    }
+//
+//    public void setCourse(double course) {
+//        this.course = course;
+//    }
+    private double x = 0;
     private double y = 0;
     protected double course = 0;
+    private Operator operator;
 
     public Robot(double x, double y) {
         this.x = x;
         this.y = y;
     }
 
-    // Передвижение на дистанцию distance
+    public Operator getOperator() {
+        return operator;
+    }
+
+    public void setOperator(Operator operator) {
+        this.operator = operator;
+    }
+
     public void forward(int distance) {
         x = x + distance * Math.cos(course / 180 * Math.PI);
         y = y + distance * Math.sin(course / 180 * Math.PI);
-    }
-
-    // Печать координат робота
-    public void printCoordinates() {
-        System.out.println(x + "," + y);
     }
 
     public double getX() {
@@ -78,5 +116,9 @@ private double x = 0;
 
     public void setCourse(double course) {
         this.course = course;
+    }
+
+    public void printCoordinates() {
+        System.out.println(x + "," + y);
     }
 }
