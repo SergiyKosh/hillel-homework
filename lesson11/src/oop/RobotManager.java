de580ff4401e5daeab57b5b9d2ce864e215bbba1
Serhii <sergey.mov8@gmail.com>
2021-11-05 16:30:14 +0200

    [HW-11-01] Modified "RobotManager" class

--- lesson11/src/oop/RobotManager.java
+++ lesson11/src/oop/RobotManager.java
@@ -34,29 +34,33 @@ public class RobotManager {
 //        // Напечатать координаты
 //        robot.printCoordinates();
 
-//        Robot robot = new RobotTotal(0, 0);
+    //        Robot robot = new RobotTotal(0, 0);
 //        System.out.println(robot.getClass().getName());
 //    }
 //
 //    private void changeCourse(Robot robot) {
 //        robot.setCourse(180);
 //    }
-        public static void main (String[]args){
-            // Количество сторон многоугольника
-            final int COUNT = 12;
-            // Длина стороны
-            final int SIDE = 100;
-
-            Robot robot = new Robot(200, 50);
-            // Создаем замкнутую фигуру с количеством углов COUNT
-            for (int i = 0; i < COUNT; i++) {
-                robot.forward(SIDE);
-                robot.setCourse(robot.getCourse() + 360 / COUNT);
-            }
-
-            // Создаем форму для отрисовки пути нашего робота
-            RobotFrame rf = new RobotFrame(robot);
-            rf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            rf.setVisible(true);
-        }
+//        public static void main (String[]args){
+//            // Количество сторон многоугольника
+//            final int COUNT = 12;
+//            // Длина стороны
+//            final int SIDE = 100;
+//
+//            Robot robot = new Robot(200, 50);
+//            // Создаем замкнутую фигуру с количеством углов COUNT
+//            for (int i = 0; i < COUNT; i++) {
+//                robot.forward(SIDE);
+//                robot.setCourse(robot.getCourse() + 360 / COUNT);
+//            }
+//
+//            // Создаем форму для отрисовки пути нашего робота
+//            RobotFrame rf = new RobotFrame(robot);
+//            rf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//            rf.setVisible(true);
+//        }
+    public static void main(String[] args) {
+        Robot[] rbts;
+        rbts = new Robot[10];
     }
+}
