0ab15f36b44ba8ae729a8e28957815ab7455caaa
Serhii <sergey.mov8@gmail.com>
2021-11-10 17:53:01 +0200

    [HW-14-02] Task is solved

---
+++ lesson14/hw2/src/flowers/Lavender.java
@@ -0,0 +1,7 @@
+package flowers;
+
+public class Lavender extends Flowers {
+    public Lavender(String name, double cost) {
+        super(name, cost);
+    }
+}
--- lesson14/hw2/src/flowers/Rose.java
+++ lesson14/hw2/src/flowers/Rose.java
@@ -1,4 +1,7 @@
 package flowers;
 
 public class Rose extends Flowers {
+    public Rose(String name, double cost) {
+        super(name, cost);
+    }
 }
