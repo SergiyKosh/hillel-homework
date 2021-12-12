24aeb5f9581981745bbee99cf0afd4c8596525f1
Serhii <sergey.mov8@gmail.com>
2021-12-12 18:17:04 +0200

    [HW-22-02] Добавьте в метод run() код создания, инициализации и вывода двух массивов типа Integer и Double. Затем отобразите в консоль результат работы обобщенного метода calcNum() на этих массивах.

--- lesson22/hw2/src/ua/ithillel/task1/Runner.java
+++ lesson22/hw2/src/ua/ithillel/task1/Runner.java
@@ -2,6 +2,9 @@ package ua.ithillel.task1;
 
 public class Runner {
     public void run() {
-
+        Integer[] intArr = {2, 3, 4, 5, 6, 7, 8, 9};
+        Double[] doubleArr = {2.5, 1.3, 4.5, 7.8, 9.0};
+        System.out.println(MyTestMethod.calcNum(intArr, 4));
+        System.out.println(MyTestMethod.calcNum(doubleArr, 2.5));
     }
 }
