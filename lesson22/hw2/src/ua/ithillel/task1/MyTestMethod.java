5bdb59d283721c8dd92d358499add2d7e2dddafe
Serhii <sergey.mov8@gmail.com>
2021-12-12 18:12:15 +0200

    [HW-22-02] Добавьте в пакет task1 класс MyTestMethod с обобщенным статическим методом calcNum(), который принимает два параметра обобщенного типа: массив и переменную value, и подсчитывает количество элементов массива со значением большим за value.

---
+++ lesson22/hw2/src/ua/ithillel/task1/MyTestMethod.java
@@ -0,0 +1,11 @@
+package ua.ithillel.task1;
+
+import java.util.Arrays;
+
+public class MyTestMethod {
+    public static <E extends Comparable<E>> long calcNum(E[] arr, E value) {
+        return Arrays.stream(arr)
+                .filter(x -> x.compareTo(value) > 0)
+                .count();
+    }
+}
