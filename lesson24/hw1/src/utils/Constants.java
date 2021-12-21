babf7b73b03b8f5e53700bddea916ef5c2218ea7
SergiyKosh <sergey.mov8@gmail.com>
2021-12-21 20:00:27 +0200

    [HW-24-01] init project and solved the task

---
+++ lesson24/hw1/src/utils/Constants.java
@@ -0,0 +1,6 @@
+package utils;
+
+public final class Constants {
+    public static final String PATH_TO_PRODUCTS = "src/resources/products.txt";
+    public static final String PATH_TO_ORDERS = "src/resources/orders.txt";
+}
